b125e48af64bf60cc60bac725a7c9503450bc050
marcomendoza <48435302+marcoamt@users.noreply.github.com>
2019-10-06 01:47:59 +0200

    Update Automator.py

--- Automator.py
+++ Automator.py
@@ -3,14 +3,18 @@ from github import Github
 
 #save commands in /Users/{username}/.my_custom_commands.sh
 
-#take the params passed by the custom commands
-folderName = str(sys.argv[2])
+def auto():
+  #take the params passed by the custom commands
+  folderName = str(sys.argv[2])
 
-username = "{git user}"
-password = "{password}"
-login = Github(username, password)
+  username = "{git user}"
+  password = "{password}"
+  login = Github(username, password)
 
-#git login
-user = login.get_user()
-#create repo on github
-user.create_repo(folderName)
+  #git login
+  user = login.get_user()
+  #create repo on github
+  user.create_repo(folderName)
+  
+if __name__ == "__main__":
+    auto()
