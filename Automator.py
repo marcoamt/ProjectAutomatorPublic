e73f5249e48f85ca86870f36877d31799045c002
marcomendoza <48435302+marcoamt@users.noreply.github.com>
2019-10-06 01:24:19 +0200

    Create Automator.py

---
+++ Automator.py
@@ -0,0 +1,16 @@
+import sys
+from github import Github
+
+#save commands in /Users/{username}/.my_custom_commands.sh
+
+#take the params passed by the custom commands
+folderName = str(sys.argv[2])
+
+username = "{git user}"
+password = "{password}"
+login = Github(username, password)
+
+#git login
+user = login.get_user()
+#create repo on github
+user.create_repo(folderName)
